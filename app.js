4b8bc2f3782c1bf005e3a9497cb073f3eb80097e
lgalant <lgalant@live.com>
2026-09-22 09:55:19 -0300

    code cleanup

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-//import pkg from 'pg'
-//import dbconfig from './dbconfig.js'
-//import bcrypt from 'bcrypt'
-//import jwt from 'jsonwebtoken'
 
 import express from 'express'
 import cors from "cors";
@@ -9,27 +5,12 @@ import cors from "cors";
 import authRouter from './Routes/authRouter.js'
 import cancionRouter from './Routes/cancionRouter.js'
 
-// Postgres Pool en vez de client, no necesita connect ni end
-//const {Pool} = pkg;
-//const pool = new Pool(dbconfig)
-
-
 
 const app = express()
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-
-
-const JWT_SECRET = 'veryverysecret!'
-
-const horaMiddleware = function (req, res, next) {
-  console.log('Middleware (Antes): ' + new Date().toISOString());
-  next(); 	// Ir al próximo middleware
-  console.log('Middleware (Despues): ' + new Date().toISOString());
-}
-//app.use(horaMiddleware);
+// Rutas
 app.use("/auth", authRouter);
 app.use("/cancion", cancionRouter);
 
@@ -47,6 +28,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
+//const PORT = process.env.PORT || 3000;
 //app.listen(PORT, () => { console.log(`Local en http://localhost:${PORT}`);});
 
 export default app;
